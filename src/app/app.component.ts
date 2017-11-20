45900213d79d8d4ea80693f657675abee57f3247
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-20 12:05:47 +0000

    Finished the nightmare that is binding to custom events.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    {type: 'server', name: 'TestServer', content: 'Just a test!'}
+  ];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    console.log('On Server added');
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    console.log('On Blueprint added');
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 
 
 }
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,40 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  @Output()
+  serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  @Output()
+  blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    console.log('onAddServer');
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    console.log('onAddBlueprint');
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+
+}
